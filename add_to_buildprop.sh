dff8344914eea8ebc8a31544e7b037b01329d505
Fusion <leemenhin@gmail.com>
2020-06-16 22:45:32 +0800

    Update add_to_buildprop.sh

--- add_to_buildprop.sh
+++ add_to_buildprop.sh
@@ -8,7 +8,7 @@ echo "  " >> system/build.prop
 echo "# Screen Mirrorring Fix" >> system/build.prop
 echo "wlan.wfd.hdcp=disable" >> system/build.prop
 
-echo "  " >> /system/build.prop
+echo "  " >> system/build.prop
 echo "#Disables Error Checking" >> system/build.prop
 echo "ro.kernel.android.checkjni=0" >> system/build.prop
 echo "ro.kernel.checkjni=0" >> system/build.prop
@@ -29,7 +29,7 @@ echo "# Boot-Animation" >> system/build.prop
 echo "boot.fps=26" >> system/build.prop
 echo "shutdown.fps=26" >> system/build.prop
 
-echo "  " >> /system/build.prop
+echo "  " >> system/build.prop
 echo "#Disable SPCM app killer" >> system/build.prop
 echo "sys.config.samp_spcm_enable=false" >> system/build.prop
 echo "sys.config.spcm_db_enable=false" >> system/build.prop
@@ -46,11 +46,11 @@ echo "  " >> system/build.prop
 echo "# Camera Tweaks" >> system/build.prop
 echo "ro.media.enc.jpeg.quality=100" >> system/build.prop
 
-echo "  " >> /system/build.prop
+echo "  " >> system/build.prop
 echo "#Allow Purge of Assets To Free Ram" >> system/build.prop
 echo "persist.sys.purgeable_assets=1" >> system/build.prop
 
-echo "  " >> /system/build.prop
+echo "  " >> system/build.prop
 echo "#Launcher Is Kept in Memory" >> system/build.prop
 echo "ro.HOME_APP_ADJ=1" >> system/build.prop
 
@@ -58,7 +58,7 @@ echo "  " >> system/build.prop
 echo "#Wifi Fix" >> system/build.prop
 echo "ro.securestorage.support=false" >> system/build.prop
 
-echo "  " >> /system/build.prop
+echo "  " >> system/build.prop
 echo "# Misc Tweaks" >> /system/build.prop
 echo "ro.com.google.locationfeatures=1" >> system/build.prop
 echo "ro.com.google.networklocation=1" >> system/build.prop
@@ -66,7 +66,7 @@ echo "touch.presure.scale=0.001" >> system/build.prop
 echo "persist.cne.feature=0" >> system/build.prop
 echo "androidboot.selinux=0" >> system/build.prop
 
-echo "  " >> /system/build.prop
+echo "  " >> system/build.prop
 echo "# Signal Tweaks" >> system/build.prop
 echo "ro.telephony.call_ring.delay=0" >> system/build.prop
 echo "ring.delay=0" >> system/build.prop
@@ -78,7 +78,7 @@ echo "wifi.supplicant_scan_interval=180" >> system/build.prop
 echo "ro.ril.disable.power.collapse=0" >> system/build.prop
 
 
-echo "  " >> /system/build.prop
+echo "  " >> system/build.prop
 echo "#SafetyNet Fix" >> system/build.prop
 echo "ro.knox.enhance.zygote.aslr=1" >> system/build.prop
 
